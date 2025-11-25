33e84b18198eba06e06b6a7a54ffefe860954c6a
Dotdan5919 <dighodaro95@gmail.com>
2025-11-25 15:14:27 +0100

    -

--- components/sections/Explore.tsx
+++ components/sections/Explore.tsx
@@ -3,7 +3,7 @@ import MyBtn from '../ui/MyBtn'
 
 export default function Explore() {
   return (
-    <div className=' w-[80%] my-4 p-20 bg-mybg flex flex-col gap-8  items-center justify-center text-center'>
+    <div className=' md:w-[80%] w-full my-4 p-20 bg-mybg flex flex-col gap-8  items-center justify-center text-center'>
 
 <h1 className='MyH5'>Experience Our Premium Products & Platform</h1>
 <p className='max-w-[600px] text-center text-mytext'>Whether you're interested in our premium food products or want to leverage our digital platform for agricultural trade, we're here to help you succeed.</p>
